ebc5980c796da6be8bd13577352142cb943e3bd1
Ferruccio Balestreri <hi@ferrucc.io>
2020-09-16 03:49:52 +0200

    reload on submit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,7 @@ class QuestionsController < ApplicationController
     before_action :authenticate_user!
     def create
         Question.create!(body: params[:body])
+        redirect_to '/questions'
     end
 
     def index
